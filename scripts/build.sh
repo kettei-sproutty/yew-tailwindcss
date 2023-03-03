1fc3a8f4af0ebf1d19921b0ce5e39bd583b826c8
alessio.marchi <alessio.marchi@accenture.com>
2023-03-03 17:55:27 +0100

    fix: manifest.json

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,3 +4,6 @@
 if [ ! -f "dist/tailwind.css" ]; then
   pnpm cross-env NODE_ENV=production postcss src/styles/main.css -o "dist/tailwind.css" --minify
 fi
+
+# Copy manifest.json (unhandled by Trunk)
+cp src/assets/manifest.json dist/manifest.json
