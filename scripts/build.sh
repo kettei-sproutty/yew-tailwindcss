7bb0524f1b0da189c35753be19ace014d4f2fa4f
alessio.marchi <alessio.marchi@accenture.com>
2023-03-03 10:16:37 +0100

    feat: add build task

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,2 @@
 echo "Install Rust, Cargo, Trunk and Build"
-amazon-linux-extras install rust1 && rustup target add wasm32-unknown-unknown && cargo install --locked trunk && trunk build --release
+amazon-linux-extras install rust1 && source $HOME/.cargo/env && rustup target add wasm32-unknown-unknown && cargo install --locked trunk && trunk build --release
