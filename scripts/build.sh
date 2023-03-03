b74e469120847d229c4a3c53e4fb6503e74c7795
alessio.marchi <alessio.marchi@accenture.com>
2023-03-03 18:45:44 +0100

    fix: remove 192x192 and 512x512 from trunk

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,16 +5,8 @@ if [ ! -f "dist/tailwind.css" ]; then
   pnpm cross-env NODE_ENV=production postcss src/styles/main.css -o "dist/tailwind.css" --minify
 fi
 
-# Copy manifest.json (unhandled by Trunk)
+# Copy unhandled assests
 cp src/assets/manifest.json dist/manifest.json
-
-# Extract the hashed name of the file
-HASH_512=$(ls dist/android-chrome-512x512-*.png | sed -e 's/^.*-\(.*\)\.png$/\1/')
-HASH_192=$(ls dist/android-chrome-192x192-*.png | sed -e 's/^.*-\(.*\)\.png$/\1/')
-
-# Replace the file names with the hashed names in the manifest.json file
-sed -i '' "s/android-chrome-512x512.png/android-chrome-512x512-$HASH_512.png/g" dist/manifest.json
-sed -i '' "s/android-chrome-192x192.png/android-chrome-192x192-$HASH_192.png/g" dist/manifest.json
-
-# Copy robots.txt
+cp src/assets/icons/android-chrome-192x192.png dist/android-chrome-192x192.png
+cp src/assets/icons/android-chrome-512x512.png dist/android-chrome-512x512.png
 cp src/assets/robots.txt dist/robots.txt
