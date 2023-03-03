d6f363825067032d86ddb2fd1365df1a922fd1d6
alessio.marchi <alessio.marchi@accenture.com>
2023-03-03 10:27:42 +0100

    feat: add build task

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 # echo "Install Rust, Cargo, Trunk and Build"
 
-curl https://sh.rustup.rs -sSf | sh -s -- -y && rustup target add wasm32-unknown-unknown && cargo install --locked trunk && trunk build --release
+export HOME=/vercel && curl https://sh.rustup.rs -sSf | sh -s -- -y && rustup target add wasm32-unknown-unknown && cargo install --locked trunk && trunk build --release
