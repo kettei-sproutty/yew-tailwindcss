30a057aa6a4ef5dcc67654e4f06787a49f9c576b
alessio.marchi <alessio.marchi@accenture.com>
2023-03-03 10:34:03 +0100

    feat: add build task

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,5 @@
 # echo "Install Rust, Cargo, Trunk and Build"
 
-yum install sudo && sudo curl https://sh.rustup.rs -sSf | sh -s -- -y && sudo rustup target add wasm32-unknown-unknown && sudo cargo install --locked trunk && trunk build --release
+amazon-linux-extras install rust1 && pnpm install
+
+rustup --version
