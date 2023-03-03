aa946e410ee8391517a1fe556131d1fcced7832f
alessio.marchi <alessio.marchi@accenture.com>
2023-03-03 18:32:09 +0100

    fix(build-script): copy hash inside manifest.json

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,5 +7,14 @@ fi
 
 # Copy manifest.json (unhandled by Trunk)
 cp src/assets/manifest.json dist/manifest.json
+
+# Extract the hashed name of the file
+HASH_512=$(ls dist/android-chrome-512x512-*.png | sed -e 's/^.*-\(.*\)\.png$/\1/')
+HASH_192=$(ls dist/android-chrome-192x192-*.png | sed -e 's/^.*-\(.*\)\.png$/\1/')
+
+# Replace the file names with the hashed names in the manifest.json file
+sed -i '' "s/android-chrome-512x512.png/android-chrome-512x512-$HASH_512.png/g" dist/manifest.json
+sed -i '' "s/android-chrome-192x192.png/android-chrome-192x192-$HASH_192.png/g" dist/manifest.json
+
 # Copy robots.txt
 cp src/assets/robots.txt dist/robots.txt
