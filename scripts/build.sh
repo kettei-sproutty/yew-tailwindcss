8d4db5ffc0e53168e8e43b00cdf19f99686e2c48
alessio.marchi <alessio.marchi@accenture.com>
2023-03-03 19:12:40 +0100

    feat(pwa): add maskable icon and apple touch icon

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,4 +9,6 @@ fi
 cp src/assets/manifest.json dist/manifest.json
 cp src/assets/icons/android-chrome-192x192.png dist/android-chrome-192x192.png
 cp src/assets/icons/android-chrome-512x512.png dist/android-chrome-512x512.png
+cp src/assets/icons/apple-touch-icon.png dist/apple-touch-icon.png
+cp src/assets/icons/maskable_icon.png dist/maskable_icon.png
 cp src/assets/robots.txt dist/robots.txt
