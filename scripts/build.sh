7c1dbc1c4aa9af611b2537ab9612cb4eec7e459c
alessio.marchi <alessio.marchi@accenture.com>
2023-03-03 11:16:23 +0100

    feat: add build task

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,6 +9,8 @@ rustup target add wasm32-unknown-unknown
 yum install wget -y
 
 wget -qO- https://github.com/thedodd/trunk/releases/download/v0.16.0/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
+echo $PATH
+source "$HOME/.cargo/env"
 
 whereis trunk
 
