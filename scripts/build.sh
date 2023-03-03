7b4d2700584903f2fa943af2c99008070f075dfb
alessio.marchi <alessio.marchi@accenture.com>
2023-03-03 10:52:41 +0100

    feat: add build task

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 source "$HOME/.cargo/env"
 
 whereis rustup
-rustup target wasm32-unknown-unknown
+rustup target add wasm32-unknown-unknown
 
 whereis rust
 whereis cargo
