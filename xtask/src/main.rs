84fd5b21e10e57e32bd944e9462a3a539db84be3
alessio.marchi <alessio.marchi@accenture.com>
2023-03-04 19:30:13 +0100

    feat: add optimization

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,44 +1,75 @@
-use std::{os::unix::process::CommandExt, process::Command};
-use xtask_wasm::{anyhow::Result, clap};
+use std::process;
+use xtask_wasm::{anyhow::Result, clap, default_build_command};
 
 #[derive(clap::Parser)]
-enum Opt {
-    Dist(xtask_wasm::Dist),
+struct Opt {
+    #[clap(long = "log", default_value = "Info")]
+    log_level: log::LevelFilter,
+    #[clap(subcommand)]
+    cmd: Command,
+}
+
+#[derive(clap::Parser)]
+enum Command {
+    Dist(Build),
     Watch(xtask_wasm::Watch),
     Start(xtask_wasm::DevServer),
 }
 
-fn build_tailwind() -> Command {
-    let mut command = Command::new("sh");
-    command.args(["-c", "pnpm build"]);
-    command.exec();
-    command
+#[derive(clap::Parser)]
+struct Build {
+    /// Optimize the generated package using `wasm-opt`.
+    #[clap(long)]
+    optimize: bool,
+
+    #[clap(flatten)]
+    base: xtask_wasm::Dist,
 }
 
 fn main() -> Result<()> {
     let opt: Opt = clap::Parser::parse();
 
-    match opt {
-        Opt::Dist(dist) => {
-            dist.dist_dir_path("apps/web/dist")
+    env_logger::builder()
+        .filter(Some("xtask"), opt.log_level)
+        .init();
+
+    let mut build_tailwind_command = process::Command::new("sh");
+    build_tailwind_command.args(["-c", "pnpm build"]);
+    build_tailwind_command.output().expect("failed to execute process");
+
+    match opt.cmd {
+        Command::Dist(arg) => {
+            log::info!("Generating package...");
+
+            let dist_result = arg
+                .base
+                .dist_dir_path("apps/web/dist")
                 .static_dir_path("apps/web/public")
+                .build_command(build_tailwind_command)
+                .build_command(default_build_command())
                 .app_name("web")
-                .build_command(build_tailwind())
                 .run_in_workspace(true)
                 .run("web")?;
+
+            if arg.optimize {
+                xtask_wasm::WasmOpt::level(1)
+                    .shrink(2)
+                    .optimize(dist_result.wasm)?;
+            }
         }
-        Opt::Watch(watch) => {
-            let mut command = Command::new("cargo");
+        Command::Watch(arg) => {
+            log::info!("Watching for changes and check...");
+
+            let mut command = process::Command::new("cargo");
             command.arg("check");
 
-            watch.run(command)?;
+            arg.run(command)?;
         }
-        Opt::Start(dev_server) => {
+        Command::Start(arg) => {
             log::info!("App ready at: {}", "127.0.0.1:8000");
-            dev_server
-                .arg("dist")
+            arg.arg("dist")
                 .start("apps/web/dist")
-                .expect("Error");
+                .expect("failed to execute process");
         }
     }
 
